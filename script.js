171a22aacc92eddb9ecd32ad61c86313a114192a
Dine5hk <99193767+Dine5hk@users.noreply.github.com>
2025-03-14 11:41:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -69,4 +69,37 @@ let questions = [
     "option 4"
     ]
   },
+     {
+    numb: 6,
+   question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+    options: [
+      "Option 1",
+      "option 2",
+    "option 3",
+    "option 4"
+    ]
+  },
+     {
+    numb: 6,
+   question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+    options: [
+      "Option 1",
+      "option 2",
+    "option 3",
+    "option 4"
+    ]
+  },
+     {
+    numb: 6,
+   question: "Your Question is Here",
+    answer: "Correct answer of the question is here",
+    options: [
+      "Option 1",
+      "option 2",
+    "option 3",
+    "option 4"
+    ]
+  },
 ];
